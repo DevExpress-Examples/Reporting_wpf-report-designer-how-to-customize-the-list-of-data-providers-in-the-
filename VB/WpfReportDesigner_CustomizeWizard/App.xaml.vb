927e6671d30d17b91b487f88beef6372527dee95
DevExpressExampleBot <fakeEmail>
2024-03-04 13:07:32 +0400

    Source auto update [skip ci]

---
+++ VB/WpfReportDesigner_CustomizeWizard/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace WpfReportDesigner_CustomizeWizard
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
--- VB/WpfReportDesigner_CustomizeWizard/MainWindow.xaml.vb
+++ VB/WpfReportDesigner_CustomizeWizard/MainWindow.xaml.vb
@@ -1,20 +1,21 @@
 Imports System.Windows
 
 Namespace WpfReportDesigner_CustomizeWizard
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-            AddHandler reportDesigner.Loaded, AddressOf ReportDesigner_Loaded
+            Me.InitializeComponent()
+            AddHandler Me.reportDesigner.Loaded, AddressOf Me.ReportDesigner_Loaded
         End Sub
 
         Private Sub ReportDesigner_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            RemoveHandler reportDesigner.Loaded, AddressOf ReportDesigner_Loaded
-            reportDesigner.ActualCommands.NewDocumentCommand.Execute(Nothing)
+            RemoveHandler Me.reportDesigner.Loaded, AddressOf Me.ReportDesigner_Loaded
+            Me.reportDesigner.ActualCommands.NewDocumentCommand.Execute(Nothing)
         End Sub
     End Class
 End Namespace
